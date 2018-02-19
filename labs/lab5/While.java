957715e4bda6a46f299f6afb11740fca70166665
P.M.Campbell <pcampbell.edu@gmail.com>
2018-02-19 17:25:35 +0000

    add while skeleton

---
+++ labs/lab5/While.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+/**
+ * 
+ * Working code skeleton for exercises to be done 
+ * with slide deck 8 - while looping
+ * 
+ * 360-420-DW Intro to Java
+ * 
+ * Examples of using a while loop & methods
+ *
+ * @author your name goes here
+ * @version  2018-02-??  (todays date goes here, delete this)
+ **/
+
+public class WhileExercise {
+    public static void main(String[] args)  {
+        // variables go here & delete or replace  this comment
+        Scanner keyboard = new Scanner(System.in);
+        
+        // logic goes here & delete or replace this comment
+        System.out.println("Brought to you by Dawson College");
+    
+    
+	} // main()
+} // WhileExercises
