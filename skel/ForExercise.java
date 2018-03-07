fad6f22975c59ddd1d5da213c39223d6daa06189
P.M.Campbell <pcampbell.edu@gmail.com>
2018-03-07 15:41:31 +0000

    skeleton for loop exercises

---
+++ skel/ForExercise.java
@@ -0,0 +1,44 @@
+
+import java.util.Scanner;
+/**
+ * 
+ * Working code skeleton for exercises to be done 
+ * with for loops and methods slide deck 11
+ *  
+ * 360-420-DW Intro to Java
+ * 
+ *
+ * @author your name goes here
+ * @version  2018-02-??  (todays date goes here, delete this text)
+ **/
+
+public class ForExercise {
+    public static void main(String[] args)  {
+        // variables go here & delete or replace  this comment
+        Scanner keyboard = new Scanner(System.in);
+        
+        // logic goes here & delete or replace this comment
+        System.out.println("Brought to you by Dawson College");
+    
+    
+	} // main()
+	
+   /**
+	* This is a method skeleton 
+	* use it to create your own methods
+	* modify this text & the @param/ @return
+	* modify the method name, parameters
+	* return type and content as needed.
+	*
+	* 360-420-DW Intro to Java
+	* @param   tbd
+	* @return  tbd
+	**/
+	public static void changeThisName() {
+	 // TODO: put your content here
+	 // TODO: add parameters as needed
+	 // TODO: change return type as needed
+	} // changeThisName()
+    
+    
+} // ForExercises
