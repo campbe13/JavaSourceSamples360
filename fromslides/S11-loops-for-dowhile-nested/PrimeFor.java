4fbccc74bd2b6593291ccfc118388e324e7f4d62
P.M.Campbell <pcampbell.edu@gmail.com>
2018-03-07 16:39:56 +0000

    add PrimeFor (uses for loop)

--- fromslides/S11-loops-for-dowhile-nested/PrimeFor.java
+++ fromslides/S11-loops-for-dowhile-nested/PrimeFor.java
@@ -3,29 +3,29 @@ import java.util.Scanner;
  * This class illustrates exercises from the slide deck for the course
  * determine if a number is prime
  * 
- * using while 
+ * using for 
  * 
  * 360-420-DW Intro to Java
  * @author PMCampbell
  * @version today
  **/
-public class Prime {
+public class PrimeFor {
     public static void main(String[] args) {
 	    Scanner kb = new Scanner(System.in);
-		boolean isPrime = true;
-		int count =2, num = 0;
+		boolean isPrime;
+		int num;
 		System.out.println("Enter numbers to test for prime, <= 0 to exit");
 		System.out.print("enter an integer: ");
 		num = kb.nextInt();
 		
+		// this is a sentinal loop, not appropriate to use for loop here
 		while (num >= 1 ) {
-			count = 2;
-			isPrime = true;	
-			while (count <= Math.sqrt(num) &&  isPrime) {
+			// cannot put this var in the for init as we use it outside
+			isPrime = true;
+			for (int count=2; count <= Math.sqrt(num) &&  isPrime; count++) {
 				if (num % count==0) {
 					isPrime = false;
 				}
-				count++;
 			}
 			if (isPrime) {
 				System.out.println(num + " is prime");
