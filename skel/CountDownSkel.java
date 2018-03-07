bea9203311a0515fd411ef552c4c58ba5e7fdbff
P.M.Campbell <pcampbell.edu@gmail.com>
2018-03-07 20:52:45 +0000

    fix skel countdown

--- skel/CountDownSkel.java
+++ skel/CountDownSkel.java
@@ -21,25 +21,42 @@ public class CountDownSkel  {
 
    // TODO limit input to between 1-100
 
-
-   // TODO a) put the countdown into a method  (limit param)
-   //   b) use a for loop instead of while
+   countDown( start );
+   
+   countDown (7);    // testing with literal value
+   
+   // Previously: put the countdown into a method  (limit param)
+   // TODO modify method
+   //   a) use a for loop instead of while
    //   c)  modify the method to have start and end params
    //   d) add a string parameter for the message
 
    // TODO create a 2nd method to count up
 
-   while (start >= 0)  {
-
-      System.out.println(start);
-      start--;
-      Thread.sleep(1000);
-
-   }
 
    System.out.println("Blastoff!");
 
    // TODO advanced ??   add Thread.sleep(1000);
 
    } // main()
+    /**
+     * This method displays the parameter and decrements it by one
+     * until it reaches 0
+     *
+     * 360-420-DW Intro to Java
+     * @author PMCampbell
+     * @version today
+     * @param num int
+     * @return void
+     **/
+ public static void countDown(int num) {
+
+         while (num >= 0) {
+
+            System.out.println(num);
+            num--;
+
+         }
+      } // countIt()
+   
 }    // CountDownSkel
