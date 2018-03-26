8e33979f53b20062c241a4c923d9bef39050a810
P.M.Campbell <pcampbell.edu@gmail.com>
2018-03-26 16:53:02 +0000

    slide deck 13 & some extras

--- skel/GuessSkel.java
+++ skel/GuessSkel.java
@@ -1,34 +1,34 @@
 import java.util.Scanner;
 
 /**
- * Working code skeleton for exercises to be done with slide deck 8 - while loopingfor the course
+ * User is to guess a random number
  *
- * 360-420-DW Intro to Java
- *
- * use a while loop with a sentinel & 
- * user guesses a number.
- *
- * @author PMCampbell
- * @version today
- **/
-public class GuessSkel {
-   public static void main(String[] args) {		Scanner kb = new Scanner(System.in);
-		// TODO: put in your own logic
-   } // main()
+ * @author     360 section 1
+ * @version    2015-03-23
+ */
+public class GuessSkell {
+    public static void main (String[] args)     {
+		int secret, randGuess;
+		boolean guessAgain = true;
+		Scanner keyboard = new Scanner(System.in);
 
-   /**
-	* This is a method skeleton 
-	* use it to create your own methods
-	* modify this text & the @param/ @return
-	* modify the method name, parameters
-	* return type and content as needed.
-	*
-	* 360-420-DW Intro to Java
-	* @param   tbd
-	* @return  tbd
-	**/
-	public static void changeThisName() {
-	 // TODO: put your content here
-	} // changeThisName()
- 
- }    // CountDown
+		// TODO: generate a secret number  (decide on a range between x & y)
+		
+		// TODO:  prompt the user to guess, if correct
+		
+		// TODO: add a cheat   (they enter 0 give them a hint)
+	
+		// TODO: maximum 3 guesses  
+	
+    	// TODO: guess out of 3 random secret numbers (use an array)
+
+		//variable declarations
+	
+		// scalingFactor = max-min+1
+		// (Math.random() * scalingFactor) + min
+    } //end main method
+} //end GuessSkell class
+
+
+
+
